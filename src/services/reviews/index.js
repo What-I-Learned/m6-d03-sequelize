da2f6eef559d828db5861c585cfeea683fa905f8
js25s <petrauskas.justas@gmail.com>
2021-09-29 16:28:46 +0200

    Create index.js

---
+++ src/services/reviews/index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import db from "../../db/models/index.js";
+import s from "sequelize";
+const { Op } = s;
+const reviewsRouter = express.Router();
+const { Product, Review } = db;
+
+reviewsRouter.get("/", async (req, res, next) => {
+  try {
+    const data = await Review.findAll({
+      include: Product,
+    });
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+reviewsRouter.get("/:id", async (req, res, next) => {
+  try {
+    const data = await Review.findByPk(req.params.id);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+reviewsRouter.post("/", async (req, res, next) => {
+  try {
+    const data = await Review.create(req.body);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+reviewsRouter.put("/:id", async (req, res, next) => {
+  try {
+    const data = await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+      returning: true,
+    });
+    res.send(data[0]);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+reviewsRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const rows = await Review.destroy({ where: { id: req.params.id } });
+    if (rows > 0) {
+      res.send("ok");
+    } else {
+      res.status(404).send("Not found");
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
